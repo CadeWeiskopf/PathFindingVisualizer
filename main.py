f48d3bf0fae46902c2a7069b7b80ec73500413a6
Cade Weiskopf <cweiskopf@thisiscsg.com>
2022-03-14 18:43:23 -1000

    writing A*... finds the path when no need to restart, needs more testing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from ast import AST
-from colorsys import hls_to_rgb
+
+from asyncio.windows_events import NULL
+from math import sqrt
 import kivy
 from kivy.app import App
 from kivy.core.window import Window
@@ -9,25 +10,127 @@ from kivy.uix.gridlayout import GridLayout
 from kivy.uix.label import Label
 import random
 
-
 class AStar():
         @staticmethod
-        def makeStep():
-                print('do makeStep')
+        def getDistance(start, end):
+                distance = sqrt(pow(end.x - start.x, 2) + pow(end.y - start.y, 2))
+                return distance
+
+        @staticmethod
+        def showPath(cameFrom, current):
+                print(' do showPath ')
 
         @staticmethod
         def solveMaze(maze):
-                step = maze.openSpaces[0].button
-                step.background_color = (255, 0, 0, 255)
-                for space in maze.openSpaces:
-                        AStar.makeStep()
+                openSet = []
+                start = maze.openSpaces[0]
+                start.button.background_color = (255, 0, 0, 255)
+                print('start '+str(start.x) +',' +str(start.y))
+                openSet.append(start)
 
+                cameFrom = []
+                
+                gScore = {start: 0}
+
+                end = maze.destinationButton
+                distance = AStar.getDistance(start, end)
+                fScore = {start: distance}
+
+                # currentCost should be in gScore?
+                currentCost = 0
+                universalCost = 1
+
+                fScoreIndex = 0
+
+                done = False
+
+                while len(openSet) > 0:
+                        current = list(fScore.keys())[fScoreIndex]
+                        print(str(current.x) + ',' + str(current.y) + ' fscoreindex' + str(fScoreIndex))
+
+                        if current == end:
+                                AStar.showPath(cameFrom, current)
+                                done = True
+
+                        if done == True:
+                                break
+
+
+                        print('remove-' + str(current.x) + ',' + str(current.y))
+                        openSet.remove(current)
+
+
+                        neighbors = []
+                        for space in maze.openSpaces:
+                                if space.x == current.x:
+                                        if current.y + 1 == space.y or current.y - 1 == space.y:
+                                                neighbors.append(space)
+                                        else:
+                                                continue
+
+                                if space.y == current.y:
+                                        if current.x - 1 == space.x or current.x + 1 == space.x:
+                                                neighbors.append(space)
+                                        else:
+                                                continue
+
+                                if current.y - 1 == space.y or current.y + 1 == space.y:
+                                        if current.x + 1 == space.x or current.x - 1 == space.x:
+                                                neighbors.append(space)
+                                        else:
+                                                continue
+
+                        tentativeHScores = {}
+                        tentativeFScores = {}
+                        tentativeGScores = {}
+                        flag = True
+                        for neighbor in neighbors:
+                                tentativeGScore = gScore[current] + universalCost
+                                tentativeHScore = AStar.getDistance(neighbor, end)
+                                tentativeFScore = tentativeGScore + tentativeHScore
+                                tentativeHScores[neighbor] = tentativeHScore
+                                tentativeFScores[neighbor] = tentativeFScore
+                                tentativeGScores[neighbor] = tentativeGScore
+                                print(str(tentativeFScore) + ' = ' + str(tentativeGScore) + ' + ' + str(tentativeHScore) +' ------------ ' + str(fScore[current]))
+                                if tentativeFScore < fScore[current]:
+                                        gScore[neighbor] = tentativeGScore
+                                        fScore[neighbor] = tentativeFScore
+                                        fScoreIndex = len(fScore.keys()) - 1
+                                        if neighbor not in openSet:
+                                                openSet.append(neighbor)
+                                        cameFrom.append(current)
+                                        current = neighbor
+                                        if current != end:
+                                                current.button.background_color = (255, 255, 255, 255)
+                                        print('new current' + str(current.x) + ',' + str(current.y))
+                                        flag = False
+                        
+                        if flag == True:
+                                low = float('inf')
+                                lowSpace = NULL
+                                for key in tentativeHScores:
+                                        if tentativeHScores[key] < low:
+                                                low = tentativeHScores[key]
+                                                lowSpace = key
+
+                                gScore[lowSpace] = tentativeGScores[lowSpace]
+                                fScore[lowSpace] = tentativeFScores[lowSpace]
+                                fScoreIndex = len(fScore.keys()) - 1
+                                if lowSpace not in openSet:
+                                        openSet.append(lowSpace)
+                                cameFrom.append(current)
+                                current = lowSpace
+                                if current != end:
+                                        current.button.background_color = (255, 255, 255, 255)
+                                print('new current flagged' + str(current.x) + ',' + str(current.y))
+                                
 
 class Maze():
         obstacles = []
         openSpaces = []
         path = []
         destination = (-1, -1)
+        destinationButton = NULL
 
         def __init__(self, rows, cols):
                 self.rows = rows
@@ -38,6 +141,7 @@ class Maze():
                 for space in self.openSpaces:
                         if space.x == x and space.y == y:
                                 space.button.background_color = (0, 255, 0, 255)
+                                self.destinationButton = space
                                 print('destination')
 
 
@@ -46,6 +150,7 @@ class GridSpace():
                 self.button = button
                 self.x = x
                 self.y = y
+
 
 class GridObstacle():
         def __init__(self, button, x, y):
@@ -68,8 +173,8 @@ class GridObstacle():
                                 else:
                                         continue
 
-                        if x + 1 == obstacle.x or x - 1 == obstacle.x:
-                                if y - 1 == obstacle.y or y + 1 == obstacle.y:
+                        if y - 1 == obstacle.y or y + 1 == obstacle.y:
+                                if x + 1 == obstacle.x or x - 1 == obstacle.x:
                                         return False
                                 else:
                                         continue
@@ -105,21 +210,19 @@ class MyGrid(GridLayout):
                         i -= 1
 
                 maze.setDestination(1, 1)
-                for ob in maze.obstacles:
-                        print(str(ob))
+                #for ob in maze.obstacles:
+                #        print(str(ob))
 
                 for os in maze.openSpaces:
-                        print(str(os))
+                        print(str(os.x) + ',' + str(os.y))
                 
                 AStar.solveMaze(maze)
 
 
-
-        
-
 class MyApp(App):
         def build(self):
                 return MyGrid()
+
 
 if __name__ == '__main__':
         myApp = MyApp()
